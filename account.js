a8d8190c501fd2d9abc6dfeb7af65756f1a2a83a
miraclew3 <64519795+miraclew3@users.noreply.github.com>
2020-06-01 15:56:29 +0800

    Update account.js

--- account.js
+++ account.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
             _number = 1;
 
         }
-	     console.log(_number);
+	if(isNAN(_number)) number = 1;
         $('#form_entry_id').val(_id);
 	    $('#form_entry_comment').val(_number);
         $('#form_entry_name').val(_name);
