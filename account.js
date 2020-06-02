0013b8ee6723361b3f5e86c1c45e6a943154802c
ward <webprofessional555@gmail.com>
2020-06-02 14:36:07 +0800

    update

--- account.js
+++ account.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     $('.fb-button').click(function(){
         _this = $(this);
         var shareurl = window.location.hostname  + _this.closest('.entry-img-div').next().next().children().attr('href');
-        var _title = _this.closest('.entry-img-div').prev().find(p).text();
+        var _title = _this.closest('.entry-img-div').prev().find('p').text();
         window.open('https://www.facebook.com/sharer/sharer.php?u='+escape(shareurl)+'&t='+_title, '', 
         'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
         return false;
