bc4be21e355de4d556e397167bb2c9f2ce485142
ward <webprofessional555@gmail.com>
2020-06-04 14:09:41 +0800

    update

--- custom.js
+++ custom.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
-    $('.comment').click(function(){
+    
+    $('.fb-button').on('click',function(){
         _this = $(this);
-        _id = _this.parent().parent().parent().prev().text();
-        _type = _this.attr('data-title');
-        $('#form_entry_id').val(_id);
-        $('#form_entry_type').val(_type);
-        $('#wf-form-comment_form').submit();
+        var shareurl = window.location.hostname  + _this.closest('.entry-img-div').next().next().children('.view-diary-btn').attr('href');
+        var _title = _this.closest('.entry-img-div').prev().find('.entry-item-title').text();
+        window.open('https://www.facebook.com/sharer/sharer.php?u='+shareurl+'&t='+_title, '', 
+        'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+        return false;        
+    });
 
+
+    $('.emoji-button').on('click',function(){
+        _this = $(this);
+        var shareurl = window.location.hostname  + _this.closest('.entry-img-div').next().next().children('.view-diary-btn').attr('href');
+        $('#copy_clip').val(shareurl);
+        $('#copy_clip').select();
+        document.execCommand("copy");
+        return false;        
     });
 })
